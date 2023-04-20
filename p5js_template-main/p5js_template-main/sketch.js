e1675e76fb4d081227be346d3712d2c2a107cad1
richabansal23 <129701105+richabansal23@users.noreply.github.com>
2023-04-20 20:02:12 +0530

    Add files via upload

---
+++ p5js_template-main/p5js_template-main/sketch.js
@@ -0,0 +1,23 @@
+
+var marks=[12,45,78,56];
+var sum=0;
+for(var i=0;i<3;i++)
+{
+
+sum=sum+marks[i];
+
+}
+console.log(sum);
+function setup() {
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background("skyblue");
+  fill('red');
+  rect(200,200,20,20);
+   
+  
+
+}
